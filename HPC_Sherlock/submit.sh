f075482023aa3651ff335b55670d50a9f618eb76
Andy <39730681+AndyStudio@users.noreply.github.com>
2024-05-22 18:03:38 -0700

    Update submit.sh

--- HPC_Sherlock/submit.sh
+++ HPC_Sherlock/submit.sh
@@ -3,11 +3,9 @@
 #SBTACH --partition=serc
 #SBATCH --time=1-22:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=4
 
 module use /home/groups/sh_s-dss/share/sdss/modules/modulefiles
 module --ignore-cache load CMG
 
-#module python/3.9
-
-python ../pyCTRLfiles/pycontrol_1.py
+python pycontrol_1.py
