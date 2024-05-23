9aa6806145a6732b89e320c10052e5ff6c5d0d5c
Andy <39730681+AndyStudio@users.noreply.github.com>
2024-05-22 18:43:16 -0700

    Update pycontrol.py

--- HPC_Sherlock/pycontrol.py
+++ HPC_Sherlock/pycontrol.py
@@ -1,6 +1,7 @@
 import os
-
+# Users need to change the dat name accordingly ...
 dat_name = 'case0.dat'
+#####
 pwd_CMG = "/home/groups/sh_s-dss/share/sdss/software/x86_64_arch/CMG/2023.101/gem/2023.10/linux_x64/exe/gm202310.exe"
 pwd = os.getcwd()
 t = 'cd ' + pwd + ' &' + str(pwd_CMG) + '  -f ' + dat_name
