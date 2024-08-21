dcf337fd55c368c36346f3007ecd2d6c0a34effa
AndyGESB159 <lyn.jyc@gmail.com>
2024-08-20 20:43:40 -0700

    new files for cmgmodel

---
+++ cmg_models/wrtcmgdat_SPR_CCS_field6x6.py
@@ -0,0 +1,11 @@
+import numpy as np 
+import os
+
+
+class Write_datfiles_SPRCCS():
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        
+        
+##### Example file provided upon request, in accordance with data policy. #####
---
+++ cmg_models/wrtcmgdat_h2_rxns.py
@@ -0,0 +1,14 @@
+import numpy as np 
+import pandas as pd
+import os
+
+
+class Write_datfiles_h2():
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.params = None
+
+    ##### Example file provided upon request, in accordance with data policy. #####
+
+  
